5fd881a1d8712b24fd59c32dad1f8e470de39f82
mangosd <mangosd@hotmail.de>
2021-12-26 16:20:31 +0100

    BarrelEnhancement

---
+++ scripts/4_world/mangoBarrelEnhancement.c
@@ -0,0 +1,21 @@
+modded class Barrel_ColorBase
+{
+    override void SetActions()
+	{
+        super.SetActions();
+    
+        AddAction(ActionWorldLiquidActionSwitch);
+	    AddAction(ActionFillFuel);
+	    AddAction(ActionFillCoolant);
+	    AddAction(ActionFillGeneratorTank);
+	    AddAction(ActionExtinguishFireplaceByLiquid);
+	    AddAction(ActionFillBottleBase);
+	    AddAction(ActionWaterGardenSlot);
+	    AddAction(ActionWaterPlant);
+	    AddAction(ActionForceDrink);
+	    AddAction(ActionTransferLiquid);
+	    AddAction(ActionEmptyBottleBase);
+	    AddAction(ActionWashHandsItem);
+	    AddAction(ActionDrink);
+    }
+}
