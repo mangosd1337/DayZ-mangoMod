6167ff357ebbbf06ced5718f6cc59bf05572b3c1
mangosd <mangosd@hotmail.de>
2021-12-27 23:20:38 +0100

    comment out testing stuff

--- scripts/config.cpp
+++ scripts/config.cpp
@@ -36,27 +36,27 @@ class CfgVehicles
 	class Inventory_Base;
 	class Container_Base;
 	class Box_Base;
-	class BurlapSack: Inventory_Base
-	{
-        //canBeSplit=1;
-        //varQuantityInit=70;
-        //varQuantityMin=0;
-        //varQuantityMax=100;
-        //varQuantityDestroyOnMin=1;
-
-		canBeDigged=1;
-
-		allowOwnedCargoManipulation=1;
-
-		class Cargo
-		{
-			itemsCargoSize[]={10,10};
-		};
-	};
+//	class BurlapSack: Inventory_Base
+//	{
+//        //canBeSplit=1;
+//        //varQuantityInit=70;
+//        //varQuantityMin=0;
+//        //varQuantityMax=100;
+//        //varQuantityDestroyOnMin=1;
+//
+//		canBeDigged=1;
+//
+//		allowOwnedCargoManipulation=1;
+//
+//		//class Cargo
+//		//{
+//		//	itemsCargoSize[]={10,10};
+//		//};
+//	};
 
 	class Barrel_ColorBase: Container_Base
 	{
-		itemSize[]={1,2};
+		//itemSize[]={1,2};
 		canBeDigged=1;
 
 		//allowOwnedCargoManipulation=1;
@@ -66,11 +66,11 @@ class CfgVehicles
 		//	"Back"
 		//};
 
-		class Cargo
-		{
-			itemsCargoSize[]={10,40};
-			allowOwnedCargoManipulation=1;
-			openable=1;
-		};
+		//class Cargo
+		//{
+		//	itemsCargoSize[]={10,40};
+		//	//allowOwnedCargoManipulation=1;
+		//	//openable=1;
+		//};
 	}
 };
